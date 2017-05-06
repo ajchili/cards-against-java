7a60bce88825f2483b42a06c2be6af5c0f7d5d52
Kirin Patel <kirinpatel@gmail.com>
2017-05-06 13:29:19 -0400

    Removed unnecessary comments and added JSON library

--- Cards Against Java/src/com/kirinpatel/Main.java
+++ Cards Against Java/src/com/kirinpatel/Main.java
@@ -1,13 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.kirinpatel;
 
-import com.kirinpatel.util.CardImporter;
-import java.io.FileNotFoundException;
-import java.util.ArrayList;
+import com.kirinpatel.net.Server;
 
 /**
  *
@@ -18,11 +11,12 @@ public class Main {
     
     public static boolean closeGame = false;
     
-    public static void main(String[] args) throws FileNotFoundException {
-        ArrayList<String> cards = CardImporter.importCards(1, 2);
-        
-        cards.forEach((card) -> {
-            System.out.println(card);
-        });
+    /**
+     * Main method.
+     * 
+     * @param args Command line arguments
+     */
+    public static void main(String[] args) {
+    	new Server();
     }
 }
