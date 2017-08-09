5e8034d2c4a87a81d94f6b4b8b6aa344387f53e6
Kirin Patel <kirinpatel@gmail.com>
2017-08-08 20:48:42 -0400

    Added cards against humanity geek card set and moved server project

---
+++ server/src/java/com/kirinpatel/cards_against_java/Cards_Against_Java.java
@@ -0,0 +1,4 @@
+package java.com.kirinpatel.cards_against_java;
+
+public class Cards_Against_Java {
+}
--- src/java/com/kirinpatel/cards_against_java_server/Cards_Against_Java.java
+++
@@ -1,4 +0,0 @@
-package java.com.kirinpatel.cards_against_java_server;
-
-public class Cards_Against_Java {
-}
